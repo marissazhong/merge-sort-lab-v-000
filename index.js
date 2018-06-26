9b939804a8301a1ae46ba5442077dd1c0595529f
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 19:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ function merge(arr1, arr2) {
   merged_arr = [];
   while (arr1.length != 0 && arr2.length != 0) {
     if (arr1[0] <= arr2[0]) {
-
+      merged_arr.push(arr1[0]);
+      arr1.splice(0,1);
+    } else {
+      merged_arr.push(arr2[0]);
+      arr2.splice(0,1);
     }
   }
   if (arr1.length != 0) {
